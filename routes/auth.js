314ada926775e91f1370c559a362f47d5c936c43
Samrendra Singh <samrendrakumarsingh01@gmail.com>
2020-09-06 00:26:16 +0530

    Added sign-in method with verification using jwt

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,72 @@ const express = require('express')
 const router =express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+const requireLogin = require('../middleware/require_login')
 
-router.get('/', (req,res)=>{
-    res.send("hello")
+router.get('/protected', requireLogin,(req,res)=>{
+    res.send('hello user')
 })
 
 router.post('/signup', (req,res)=>{
     const {name, email, password} = req.body
     if(!email || !password || !name){
-        res.status(422).json({error:"please add all the required fields"})
+        return res.status(422).json({error:"please add all the required fields"})
     }
-    res.json({message:"Successfully posted"})
+    User.findOne({email: email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"User already exists with that email"})
+        }
+        bcrypt.hash(password, 12)
+        .then(hashedPassword=>{
+            const user = new User({
+                email,
+                password: hashedPassword ,
+                name
+            })
+            
+            user.save()
+            .then(user=>{
+                res.json({message: "saved successfully"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+router.post('/signin', (req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).json({error:'Please provide email or passwords'})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+            return res.status(422).json({error:"Invalid Credentials"})
+        }
+        bcrypt.compare(password, savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                // return res.json({message:"Successfully signed in"})
+                const token = jwt.sign({_id: savedUser._id}, JWT_SECRET)
+                res.json({token})
+            }
+            else{
+                return res.status(422).json({error:"Invalid Email or Password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
 })
 
 module.exports = router
